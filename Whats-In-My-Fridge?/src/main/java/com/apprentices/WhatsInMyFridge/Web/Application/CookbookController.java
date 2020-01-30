0f83b22f512d9ef71298adc620c129f6f0dbf547
Johanna Longmuir <johannalongmuir@gmail.com>
2020-01-30 09:33:47 +0000

    Response Headers added from backend

--- Whats-In-My-Fridge?/src/main/java/com/apprentices/WhatsInMyFridge/Web/Application/CookbookController.java
+++ Whats-In-My-Fridge?/src/main/java/com/apprentices/WhatsInMyFridge/Web/Application/CookbookController.java
@@ -3,6 +3,9 @@ package com.apprentices.WhatsInMyFridge.Web.Application;
 import com.apprentices.WhatsInMyFridge.Entity.Recipe;
 import com.apprentices.WhatsInMyFridge.Service.CookbookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +21,10 @@ public class CookbookController {
 
     @GetMapping("/{ingredients}")
     public @ResponseBody
-    List<Recipe> getRecipesByIngredients(@PathVariable String ingredients) throws IOException {
-        return cookbookService.getRecipesFromIngredients(ingredients);
+    ResponseEntity<List<Recipe>> getRecipesByIngredients(@PathVariable String ingredients) throws IOException {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Access-Control-Allow-Origin", "*");
+        return new ResponseEntity<>(cookbookService.getRecipesFromIngredients(ingredients), responseHeaders, HttpStatus.OK);
     }
 
 }
